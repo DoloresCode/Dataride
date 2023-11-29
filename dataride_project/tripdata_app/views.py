f5f4d0521778cdbe827e2c4a0d246b7b97931fb2
Dolores Crazover <doloresnetweb@gmail.com>
2023-11-29 00:51:09 -0500

    api-results view modified, api-results.html and api-tester.html file created and styled a little as welle as bugs fixed

--- dataride_project/tripdata_app/views.py
+++ dataride_project/tripdata_app/views.py
@@ -10,8 +10,28 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = 'about.html'
 
+
 def interactive_form(request):
     return render(request, 'api-tester.html')
 
+
 def api_results(request):
-    return render(request, 'api-results.html')
+    # retrive query parqmeteres (station id and if no provided default empty string + nbr of entries of 10 by default) from the request.
+    station_id = request.GET.get('station_id', '')
+    number_of_entries = request.GET.get('number_of_entries', 10)
+
+    # we initialize an empty list of trips to store the trip data (results) we will fetch from the database.
+    trips = []
+
+    if station_id:
+        try:
+            number_of_entries = int(number_of_entries)  # Convert to integer
+        except ValueError:
+            number_of_entries = 10  # Default to 10 if conversion fails
+
+        # Query the database for the first 10 trips that start at the station with the given station_id. It uses Django’s ORM (Object-Relational Mapping) to interact with the database.
+        trips = TripData.objects.filter(start_station_id=station_id)[:number_of_entries]
+    
+    # render the response
+    return render(request, 'api-results.html', {'station_id': station_id, 'data': trips})
+
